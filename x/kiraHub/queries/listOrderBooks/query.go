7a16f1bc3e9028e228c03b088edf0f43d41b1122
0xjingle <dbkey.one1@gmail.com>
2020-05-15 20:54:20 +0530

    Defined Query

--- x/kiraHub/queries/listOrderBooks/query.go
+++ x/kiraHub/queries/listOrderBooks/query.go
@@ -1,29 +1,9 @@
 package listOrderBooks
 
-import (
-	"github.com/TsukiCore/cosmos-sdk/codec"
-	sdk "github.com/TsukiCore/cosmos-sdk/types"
-	sdkerrors "github.com/TsukiCore/cosmos-sdk/types/errors"
-	"github.com/TsukiCore/cosmos-sdk/x/mint"
-	abci "github.com/tendermint/tendermint/abci/types"
-)
+type QueryListOrderBooks struct {
+	index int `json:"owner"`
+}
 
-func QueryListOrderBooks(ctx sdk.Context, path []string, req abci.RequestQuery, keeper mint.Keeper) ([]byte, error) {
-	var owner, err = sdk.AccAddressFromBech32(path[0])
-	if err != nil {
-		return nil, err
-	}
-
-	coin := keeper.GetCoin(ctx, owner)
-
-	if coin.Owner == nil {
-		return []byte{}, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "could not get coin - unknown owner")
-	}
-
-	res, marshalJSONIndentError := codec.MarshalJSONIndent(packageCodec, coin)
-	if marshalJSONIndentError != nil {
-		panic(marshalJSONIndentError)
-	}
-
-	return res, nil
+func (r QueryListOrderBooks) String() int {
+	return r.index
 }
