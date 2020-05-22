c9e6ce25bda3db222e2ae3d6a02dd6df893662e1
0xjingle <dbkey.one1@gmail.com>
2020-05-23 02:32:02 +0530

    Initiated createOrder Transaction

---
+++ x/kiraHub/transactions/createOrder/codec.go
@@ -0,0 +1,15 @@
+package createOrder
+
+import (
+	"github.com/TsukiCore/cosmos-sdk/codec"
+)
+
+func RegisterCodec(codec *codec.Codec) {
+	codec.RegisterConcrete(Message{}, "tsukiHub/createOrderBook", nil)
+}
+
+var PackageCodec = codec.New()
+
+func init() {
+	RegisterCodec(PackageCodec)
+}
