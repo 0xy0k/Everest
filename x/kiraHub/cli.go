d3eba98882033873d933b70f3454741e39782a9e
0xjingle <dbkey.one1@gmail.com>
2020-06-25 15:25:28 +0530

    registering cli cmd

--- x/kiraHub/cli.go
+++ x/kiraHub/cli.go
@@ -4,6 +4,7 @@ package tsukiHub
 import (
 	constants "github.com/TsukiCore/tsuki/x/tsukiHub/constants"
 	"github.com/TsukiCore/tsuki/x/tsukiHub/queries/listOrderBooks"
+	"github.com/TsukiCore/tsuki/x/tsukiHub/queries/listOrders"
 	"github.com/TsukiCore/tsuki/x/tsukiHub/transactions/createOrderBook"
 	"github.com/TsukiCore/tsuki/x/tsukiHub/transactions/createOrder"
 	"github.com/spf13/cobra"
@@ -39,6 +40,7 @@ func GetCLIRootQueryCommand(codec *codec.Codec) *cobra.Command {
 	rootQueryCommand.AddCommand(flags.GetCommands(
 		listOrderBooks.GetOrderBooksCmd(codec),
 		listOrderBooks.GetOrderBooksByTPCmd(codec),
+		listOrders.GetOrdersCmd(codec),
 	)...)
 	return rootQueryCommand
 }
