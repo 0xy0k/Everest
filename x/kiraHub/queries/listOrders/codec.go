8002dd44cb0a042ab62c1ff2dd4022b39f54b934
0xjingle <dbkey.one1@gmail.com>
2020-06-25 15:30:56 +0530

    registering codec for orders cli

---
+++ x/kiraHub/queries/listOrders/codec.go
@@ -0,0 +1,17 @@
+package listOrders
+
+import (
+	"github.com/TsukiCore/cosmos-sdk/codec"
+)
+
+
+func RegisterCodec(cdc *codec.Codec) {
+	cdc.RegisterConcrete(QueryListOrders{}, "tsukiHub/query", nil)
+}
+
+var packageCodec = codec.New()
+
+func init() {
+	RegisterCodec(packageCodec)
+	packageCodec.Seal()
+}
