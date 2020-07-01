5f0644f9e4917749fd4ad01ff212578a879754a1
0xjingle <dbkey.one1@gmail.com>
2020-07-01 22:53:12 +0530

    minor change

--- x/kiraHub/codec.go
+++ x/kiraHub/codec.go
@@ -4,11 +4,13 @@ import (
 	"github.com/TsukiCore/cosmos-sdk/codec"
 
 	"github.com/TsukiCore/tsuki/x/tsukiHub/transactions/createOrderBook"
+	"github.com/TsukiCore/tsuki/x/tsukiHub/transactions/createOrder"
 )
 
 // RegisterCodec registers concrete types on codec
 func RegisterCodec(cdc *codec.Codec) {
 	createOrderBook.RegisterCodec(cdc)
+	createOrder.RegisterCodec(cdc)
 }
 
 // ModuleCdc defines the module codec
