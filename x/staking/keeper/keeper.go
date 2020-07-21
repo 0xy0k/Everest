3887ab7bb88ba5a6c9c1c206799acb0ddcb4a413
0xjingle <dbkey.one1@gmail.com>
2020-07-21 23:38:31 +0200

    Add new message.

--- x/staking/keeper/keeper.go
+++ x/staking/keeper/keeper.go
@@ -3,19 +3,20 @@ package keeper
 import (
 	"github.com/TsukiCore/cosmos-sdk/codec"
 	sdk "github.com/TsukiCore/cosmos-sdk/types"
-	types2 "github.com/TsukiCore/tsuki/x/staking/types"
+
+	"github.com/TsukiCore/tsuki/x/staking/types"
 )
 
 // Keeper represents the keeper that maintains the Validator Registry.
 type Keeper struct {
 	storeKey sdk.StoreKey
-	cdc      codec.Marshaler
+	cdc      *codec.Codec
 }
 
 // NewKeeper returns new keeper.
-func NewKeeper(storeKey sdk.StoreKey, cdc codec.Marshaler) *Keeper {
+func NewKeeper(storeKey sdk.StoreKey, cdc *codec.Codec) *Keeper {
 	return &Keeper{storeKey: storeKey, cdc: cdc}
 }
 
-func (k Keeper) AddValidator(ctx sdk.Context, validator types2.Validator) {
+func (k Keeper) AddValidator(ctx sdk.Context, validator types.Validator) {
 }
