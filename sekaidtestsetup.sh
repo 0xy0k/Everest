23b43621ea7d0b7c2cd4ed064866935829f04857
0xjingle <dbkey.one1@gmail.com>
2020-07-30 12:57:51 +0200

    update cosmos sdk

--- sekaidtestsetup.sh
+++ sekaidtestsetup.sh
@@ -8,6 +8,6 @@ cd $HOME
 tsukid init --chain-id=testing testing
 tsukid keys add validator
 tsukid add-genesis-account $(tsukid keys show validator -a) 1000000000stake,1000000000validatortoken
-tsukid gentx validator --chain-id=testing
+tsukid gentx validator --chain-id=testing --offline
 tsukid collect-gentxs
 tsukid start
