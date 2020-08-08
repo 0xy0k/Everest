da4586d98698d6869b6e87532373f1eab9e7c62c
0xjingle <dbkey.one1@gmail.com>
2020-08-08 13:02:30 +0200

    create querier for validator address

--- x/staking/querier.go
+++ x/staking/querier.go
@@ -3,6 +3,8 @@ package staking
 import (
 	"context"
 
+	sdk "github.com/TsukiCore/cosmos-sdk/types"
+
 	"github.com/TsukiCore/tsuki/x/staking/keeper"
 	"github.com/TsukiCore/tsuki/x/staking/types"
 )
@@ -11,6 +13,14 @@ type Querier struct {
 	keeper keeper.Keeper
 }
 
+func NewQuerier(keeper keeper.Keeper) *Querier {
+	return &Querier{keeper: keeper}
+}
+
 func (q Querier) ValidatorByAddress(ctx context.Context, request *types.ValidatorByAddressRequest) (*types.ValidatorByAddressResponse, error) {
-	panic("implement me")
+	c := sdk.UnwrapSDKContext(ctx)
+
+	validator := q.keeper.GetValidator(c, request.ValAddr)
+
+	return &types.ValidatorByAddressResponse{Validator: validator}, nil
 }
