e33bbd3434a6e3f68a2e7e90dc9293a262a0dbbb
0xjingle <dbkey.one1@gmail.com>
2020-08-14 22:19:09 +0200

    remove script to init network

--- sekaidtestsetup.sh
+++ sekaidtestsetup.sh
@@ -7,8 +7,5 @@ cd $HOME
 tsukid init --chain-id=testing testing --home=/Users/jgimeno/.tsukid
 tsukid keys add validator
 tsukid add-genesis-account $(tsukid keys show validator -a) 1000000000stake,1000000000validatortoken
-#tsukid gentx validator --chain-id=testing --offline
-#tsukid collect-gentxs
-
 tsukid gentx-claim validator
 tsukid start --home=/Users/jgimeno/.tsukid
