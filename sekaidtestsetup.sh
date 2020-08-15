dae7803a219ae1c53ae058d5fbf472dda9f3c97c
0xjingle <dbkey.one1@gmail.com>
2020-08-15 22:07:55 +0200

    update some parts of the code

--- sekaidtestsetup.sh
+++ sekaidtestsetup.sh
@@ -4,8 +4,8 @@ rm -rf $HOME/.tsukid/
 
 cd $HOME
 
-tsukid init --chain-id=testing testing --home=/Users/jgimeno/.tsukid
+tsukid init --chain-id=testing testing
 tsukid keys add validator
 tsukid add-genesis-account $(tsukid keys show validator -a) 1000000000stake,1000000000validatortoken
 tsukid gentx-claim validator
-tsukid start --home=/Users/jgimeno/.tsukid
+tsukid start
