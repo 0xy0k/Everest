62c79657b6cb5b71bac810f4e26042e475830bcf
0xjingle <dbkey.one1@gmail.com>
2020-08-25 22:20:00 +0200

    add test

--- sekaidtestsetup.sh
+++ sekaidtestsetup.sh
@@ -7,5 +7,5 @@ cd $HOME
 tsukid init --chain-id=testing testing
 tsukid keys add validator --keyring-backend=test
 tsukid add-genesis-account $(tsukid keys show validator -a --keyring-backend=test) 1000000000stake,1000000000validatortoken --keyring-backend=test
-tsukid gentx-claim validator --keyring-backend=test
+tsukid gentx-claim validator --keyring-backend=test --moniker="hello"
 tsukid start
