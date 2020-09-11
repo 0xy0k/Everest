a1e7b8711693b8fd64bfe5ea03c8a753afebb00a
0xjingle <dbkey.one1@gmail.com>
2020-09-12 02:07:08 +1000

    update response format & signature

--- INTERX/config/main.go
+++ INTERX/config/main.go
@@ -4,15 +4,22 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-var (
-	AccountAddressPrefix   = "tsuki"
-	AccountPubKeyPrefix    = "tsukipub"
+const (
+	// AccountAddressPrefix is the prefix for account address
+	AccountAddressPrefix = "tsuki"
+	// AccountPubKeyPrefix is the prefix for account public key
+	AccountPubKeyPrefix = "tsukipub"
+	// ValidatorAddressPrefix is the prefix for validator address
 	ValidatorAddressPrefix = "tsukivaloper"
-	ValidatorPubKeyPrefix  = "tsukivaloperpub"
-	ConsNodeAddressPrefix  = "tsukivalcons"
-	ConsNodePubKeyPrefix   = "tsukivalconspub"
+	// ValidatorPubKeyPrefix is the prefix for validator public key
+	ValidatorPubKeyPrefix = "tsukivaloperpub"
+	// ConsNodeAddressPrefix is the prefix for cons node address
+	ConsNodeAddressPrefix = "tsukivalcons"
+	// ConsNodePubKeyPrefix is the prefix for cons node public key
+	ConsNodePubKeyPrefix = "tsukivalconspub"
 )
 
+// SetConfig is a function to set configuration for cosmos sdk
 func SetConfig() {
 	config := sdk.GetConfig()
 	config.SetBech32PrefixForAccount(AccountAddressPrefix, AccountPubKeyPrefix)
