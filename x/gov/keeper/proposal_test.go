da22b73bf22630f7677c4c4eb88f0e3714e36168
0xjingle <dbkey.one1@gmail.com>
2020-09-27 23:36:19 +0200

    Now we can save proposal in keeper.

--- x/gov/keeper/proposal_test.go
+++ x/gov/keeper/proposal_test.go
@@ -3,6 +3,9 @@ package keeper_test
 import (
 	"testing"
 
+	"github.com/TsukiCore/tsuki/x/gov/types"
+	types2 "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/stretchr/testify/require"
 
 	"github.com/TsukiCore/tsuki/simapp"
@@ -13,7 +16,30 @@ func TestDefaultProposalIdAtDefaultGenesis(t *testing.T) {
 	app := simapp.Setup(false)
 	ctx := app.NewContext(false, tmproto.Header{})
 
-	proposalID, err := app.CustomGovKeeper.GetProposalID(ctx)
+	proposalID, err := app.CustomGovKeeper.GetNextProposalID(ctx)
 	require.NoError(t, err)
 	require.Equal(t, uint64(1), proposalID)
+}
+
+func TestSaveProposalReturnsTheProposalID_AndIncreasesLast(t *testing.T) {
+	app := simapp.Setup(false)
+	ctx := app.NewContext(false, tmproto.Header{})
+
+	proposalID, err := app.CustomGovKeeper.GetNextProposalID(ctx)
+	require.NoError(t, err)
+	require.Equal(t, uint64(1), proposalID)
+
+	addrs := simapp.AddTestAddrsIncremental(app, ctx, 1, types2.TokensFromConsensusPower(10))
+	addr := addrs[0]
+
+	proposal := types.NewProposalAssignPermission(addr, types.PermClaimValidator)
+	saveProposal, err := app.CustomGovKeeper.SaveProposal(ctx, proposal)
+	require.NoError(t, err)
+
+	require.Equal(t, uint64(1), saveProposal)
+
+	// nextProposalID should be 2
+	proposalID, err = app.CustomGovKeeper.GetNextProposalID(ctx)
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), proposalID)
 }
