c469e680ee389b2fbdec9be00b9bcb5cafdbc7b2
0xjingle <dbkey.one1@gmail.com>
2020-12-22 00:07:01 +1000

    add interx pubkey in status api

--- INTERX/gateway/kira.query.go
+++ INTERX/gateway/kira.query.go
@@ -10,16 +10,13 @@ import (
 
 const (
 	queryTsukiFunctions = "/api/tsuki/metadata"
-	queryStatus        = "/api/cosmos/status"
 )
 
 // RegisterTsukiQueryRoutes registers tx query routers.
 func RegisterTsukiQueryRoutes(r *mux.Router, gwCosmosmux *runtime.ServeMux, rpcAddr string) {
 	r.HandleFunc(queryTsukiFunctions, QueryTsukiFunctions(rpcAddr)).Methods(GET)
-	r.HandleFunc(queryStatus, QueryStatusRequest(rpcAddr)).Methods(GET)
 
 	AddRPCMethod(GET, queryTsukiFunctions, "This is an API to query tsuki functions and metadata.", true)
-	AddRPCMethod(GET, queryStatus, "This is an API to query status.", true)
 }
 
 func queryTsukiFunctionsHandle(rpcAddr string) (interface{}, interface{}, int) {
@@ -38,35 +35,5 @@ func QueryTsukiFunctions(rpcAddr string) http.HandlerFunc {
 		response.Response, response.Error, statusCode = queryTsukiFunctionsHandle(rpcAddr)
 
 		WrapResponse(w, request, *response, statusCode, false)
-	}
-}
-
-func queryStatusHandle(rpcAddr string) (interface{}, interface{}, int) {
-	return MakeGetRequest(rpcAddr, "/status", "")
-}
-
-// QueryStatusRequest is a function to query status.
-func QueryStatusRequest(rpcAddr string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		request := GetInterxRequest(r)
-		response := GetResponseFormat(request, rpcAddr)
-		statusCode := http.StatusOK
-
-		if !rpcMethods[GET][queryStatus].Enabled {
-			response.Response, response.Error, statusCode = ServeError(0, "", "API disabled", http.StatusForbidden)
-		} else {
-			if rpcMethods[GET][queryStatus].CachingEnabled {
-				found, cacheResponse, cacheError, cacheStatus := SearchCache(request, response)
-				if found {
-					response.Response, response.Error, statusCode = cacheResponse, cacheError, cacheStatus
-					WrapResponse(w, request, *response, statusCode, false)
-					return
-				}
-			}
-
-			response.Response, response.Error, statusCode = queryStatusHandle(rpcAddr)
-		}
-
-		WrapResponse(w, request, *response, statusCode, rpcMethods[GET][queryStatus].CachingEnabled)
 	}
 }
