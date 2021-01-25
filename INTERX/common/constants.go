779ad288943d43c6b155eddb0497544928cdf56b
0xjingle <dbkey.one1@gmail.com>
2021-01-25 13:06:54 +1000

    update status API

--- INTERX/common/constants.go
+++ INTERX/common/constants.go
@@ -1,25 +1,29 @@
 package common
 
 const (
-	QueryAccounts          = "/api/cosmos/auth/accounts/{address}"
-	QueryTotalSupply       = "/api/cosmos/bank/supply"
-	QueryBalances          = "/api/cosmos/bank/balances"
-	PostTransaction        = "/api/cosmos/txs"
-	QueryTransactionHash   = "/api/cosmos/txs"
-	EncodeTransaction      = "/api/cosmos/txs/encode"
-	FaucetRequestURL       = "/api/faucet"
-	QueryRPCMethods        = "/api/rpc_methods"
-	QueryInterxFunctions   = "/api/metadata"
-	QueryStatus            = "/api/status"
-	QueryWithdraws         = "/api/withdraws"
-	QueryDeposits          = "/api/deposits"
+	QueryAccounts        = "/api/cosmos/auth/accounts/{address}"
+	QueryTotalSupply     = "/api/cosmos/bank/supply"
+	QueryBalances        = "/api/cosmos/bank/balances"
+	PostTransaction      = "/api/cosmos/txs"
+	QueryTransactionHash = "/api/cosmos/txs"
+	EncodeTransaction    = "/api/cosmos/txs/encode"
+
 	QueryDataReferenceKeys = "/api/tsuki/gov/data_keys"
 	QueryDataReference     = "/api/tsuki/gov/data"
 	QueryTsukiFunctions     = "/api/tsuki/metadata"
-	Download               = "/download"
-	QueryValidators        = "/api/valopers"
-	QueryGenesis           = "/api/genesis"
-	QueryGenesisSum        = "/api/gensum"
+	QueryTsukiStatus        = "/api/tsuki/status"
+
+	FaucetRequestURL     = "/api/faucet"
+	QueryRPCMethods      = "/api/rpc_methods"
+	QueryInterxFunctions = "/api/metadata"
+	QueryWithdraws       = "/api/withdraws"
+	QueryDeposits        = "/api/deposits"
+	QueryStatus          = "/api/status"
+	QueryValidators      = "/api/valopers"
+	QueryGenesis         = "/api/genesis"
+	QueryGenesisSum      = "/api/gensum"
+
+	Download = "/download"
 
 	DataReferenceRegistry = "DRR"
 )
