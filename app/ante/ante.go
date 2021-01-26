8a66fa6850e9af7d06bf1d12dd2a73a60391db33
0xjingle <dbkey.one1@gmail.com>
2021-01-27 03:09:05 +1000

    Update ante handler for poor network messages validation decorator

--- app/ante/ante.go
+++ app/ante/ante.go
@@ -3,9 +3,9 @@ package ante
 import (
 	"fmt"
 
-	tsukitypes "github.com/TsukiCore/tsuki/types"
 	feeprocessingkeeper "github.com/TsukiCore/tsuki/x/feeprocessing/keeper"
 	customgovkeeper "github.com/TsukiCore/tsuki/x/gov/keeper"
+	customgovtypes "github.com/TsukiCore/tsuki/x/gov/types"
 	customstakingkeeper "github.com/TsukiCore/tsuki/x/staking/keeper"
 	tokenskeeper "github.com/TsukiCore/tsuki/x/tokens/keeper"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -44,7 +44,7 @@ func NewAnteHandler(
 		ante.NewValidateSigCountDecorator(ak),
 		ante.NewDeductFeeDecorator(ak, fk),
 		// poor network management decorator
-		NewPoorNetworkManagementDecorator(ak, cgk, fk),
+		NewPoorNetworkManagementDecorator(ak, cgk, sk),
 		// custom execution fee consume decorator
 		NewExecutionFeeRegistrationDecorator(ak, cgk, fk),
 		ante.NewSigGasConsumeDecorator(ak, sigGasConsumer),
@@ -161,16 +161,25 @@ func (sgcd ExecutionFeeRegistrationDecorator) AnteHandle(ctx sdk.Context, tx sdk
 type PoorNetworkManagementDecorator struct {
 	ak  keeper.AccountKeeper
 	cgk customgovkeeper.Keeper
-	fk  feeprocessingkeeper.Keeper
+	csk customstakingkeeper.Keeper
 }
 
 // NewPoorNetworkManagementDecorator returns instance of CustomExecutionFeeConsumeDecorator
-func NewPoorNetworkManagementDecorator(ak keeper.AccountKeeper, cgk customgovkeeper.Keeper, fk feeprocessingkeeper.Keeper) PoorNetworkManagementDecorator {
+func NewPoorNetworkManagementDecorator(ak keeper.AccountKeeper, cgk customgovkeeper.Keeper, csk customstakingkeeper.Keeper) PoorNetworkManagementDecorator {
 	return PoorNetworkManagementDecorator{
 		ak,
 		cgk,
-		fk,
+		csk,
 	}
+}
+
+func findString(a []string, x string) int {
+	for i, n := range a {
+		if x == n {
+			return i
+		}
+	}
+	return -1
 }
 
 // AnteHandle handle NewPoorNetworkManagementDecorator
@@ -182,32 +191,20 @@ func (pnmd PoorNetworkManagementDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx
 
 	// TODO if not poor network, skip this process
 	// handle messages on poor network
+	poorNetworkMsgs, found := pnmd.cgk.GetPoorNetworkMsgs(ctx)
+	if !found {
+		poorNetworkMsgs = &customgovtypes.AllowedMessages{}
+	}
 	for _, msg := range sigTx.GetMsgs() {
-		switch msg.Type() {
-		case bank.TypeMsgSend:
+		if msg.Type() == bank.TypeMsgSend {
 			// on poor network, we introduce POOR_NETWORK_MAX_BANK_TX_SEND network property to limit transaction send amount
 			// TODO: we could do restriction to send only when target account does not exist on chain yet for more restriction
-		default:
-			// for msgs except BankSend, it's allowance is managed by governance allowed messages array
-		case tsukitypes.MsgTypeProposalAssignPermission:
-		case tsukitypes.MsgTypeProposalSetNetworkProperty:
-		case tsukitypes.MsgTypeSetNetworkProperties:
-		case tsukitypes.MsgTypeVoteProposal:
-		case tsukitypes.MsgTypeClaimCouncilor:
-		case tsukitypes.MsgTypeWhitelistPermissions:
-		case tsukitypes.MsgTypeBlacklistPermissions:
-		case tsukitypes.MsgTypeCreateRole:
-		case tsukitypes.MsgTypeAssignRole:
-		case tsukitypes.MsgTypeRemoveRole:
-		case tsukitypes.MsgTypeWhitelistRolePermission:
-		case tsukitypes.MsgTypeBlacklistRolePermission:
-		case tsukitypes.MsgTypeRemoveWhitelistRolePermission:
-		case tsukitypes.MsgTypeRemoveBlacklistRolePermission:
-		case tsukitypes.MsgTypeClaimValidator:
-		case tsukitypes.MsgTypeActivate:
-		case tsukitypes.MsgTypePause:
-		case tsukitypes.MsgTypeUnpause:
+			return next(ctx, tx, simulate)
 		}
+		if findString(poorNetworkMsgs.Messages, msg.Type()) >= 0 {
+			return next(ctx, tx, simulate)
+		}
+		return ctx, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid transaction type on poor network")
 	}
 
 	return next(ctx, tx, simulate)
