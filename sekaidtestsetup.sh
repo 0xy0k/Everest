9fbb76f4d1117453c8ef92f204af92b2e565a380
0xjingle <dbkey.one1@gmail.com>
2021-03-09 23:23:29 +1000

    update README for frozen, fix frozen commands, register msg type and function meta

--- sekaidtestsetup.sh
+++ sekaidtestsetup.sh
@@ -6,7 +6,7 @@ cd $HOME
 
 tsukid init --chain-id=testing testing --home=$HOME/.tsukid
 tsukid keys add validator --keyring-backend=test --home=$HOME/.tsukid
-tsukid add-genesis-account $(tsukid keys show validator -a --keyring-backend=test --home=$HOME/.tsukid) 1000000000ukex,1000000000validatortoken,1000000000stake  --home=$HOME/.tsukid
+tsukid add-genesis-account $(tsukid keys show validator -a --keyring-backend=test --home=$HOME/.tsukid) 1000000000ukex,1000000000validatortoken,1000000000stake,10000000frozen  --home=$HOME/.tsukid
 tsukid gentx-claim validator --keyring-backend=test --moniker="hello" --home=$HOME/.tsukid
 tsukid start --home=$HOME/.tsukid
 
