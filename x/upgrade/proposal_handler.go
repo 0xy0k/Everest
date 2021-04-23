f9aeda64309cdc00b8fcb2ff22f3a73a9322ae06
0xjingle <dbkey.one1@gmail.com>
2021-04-23 21:04:31 +0530

    add upgrade module

---
+++ x/upgrade/proposal_handler.go
@@ -0,0 +1,29 @@
+package upgrade
+
+import (
+	"github.com/TsukiCore/tsuki/x/gov/types"
+	"github.com/TsukiCore/tsuki/x/upgrade/keeper"
+	upgradetypes "github.com/TsukiCore/tsuki/x/upgrade/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+type ApplySoftwareUpgradeProposalHandler struct {
+	keeper keeper.Keeper
+}
+
+func NewApplySoftwareUpgradeProposalHandler(keeper keeper.Keeper) *ApplySoftwareUpgradeProposalHandler {
+	return &ApplySoftwareUpgradeProposalHandler{
+		keeper: keeper,
+	}
+}
+
+func (a ApplySoftwareUpgradeProposalHandler) ProposalType() string {
+	return upgradetypes.ProposalTypeSoftwareUpgrade
+}
+
+func (a ApplySoftwareUpgradeProposalHandler) Apply(ctx sdk.Context, proposal types.Content) {
+	p := proposal.(*upgradetypes.ProposalSoftwareUpgrade)
+
+	plan := upgradetypes.NewUpgradePlan(p.MinHaltTime, p.MaxEnrolmentDuration, p.Resources.Id, p.RollbackChecksum)
+	a.keeper.ApplyUpgradePlan(ctx, plan)
+}
