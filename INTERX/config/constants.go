7604e3d3e7e024d6a2b95176236596c711c450f4
0xjingle <dbkey.one1@gmail.com>
2021-09-13 08:19:23 -0400

    add net info query api

--- INTERX/config/constants.go
+++ INTERX/config/constants.go
@@ -44,6 +44,7 @@ const (
 	QueryInterxList          = "/api/interx_list"
 	QuerySnapList            = "/api/snap_list"
 	QueryAddrBook            = "/api/addrbook"
+	QueryNetInfo             = "/api/net_info"
 
 	QueryUpgradePlan = "/api/tsuki/upgrade/current_plan"
 
