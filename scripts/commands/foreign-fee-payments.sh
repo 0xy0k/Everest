b0fbd68348e0a873972a40f60c0c19e509b14d4b
0xjingle <dbkey.one1@gmail.com>
2021-10-05 00:35:00 +1000

    add changes for scripts while testing

--- scripts/commands/foreign-fee-payments.sh
+++ scripts/commands/foreign-fee-payments.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # register stake token as 1ukex=100stake
+PermUpsertTokenRate=8
 tsukid tx customgov permission whitelist-permission --from validator --keyring-backend=test --permission=$PermUpsertTokenRate --addr=$(tsukid keys show -a validator --keyring-backend=test --home=$HOME/.tsukid) --chain-id=testing --fees=100ukex --home=$HOME/.tsukid --yes
 tsukid tx tokens upsert-rate --from validator --keyring-backend=test --denom="stake" --rate="0.01" --fee_payments=true --chain-id=testing --fees=100ukex --home=$HOME/.tsukid  --yes
 tsukid query tokens rate stake
