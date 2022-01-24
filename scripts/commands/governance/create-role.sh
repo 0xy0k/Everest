8f05d654c8caa0bca1cda0247ed9d6308a71b242
0xjingle <dbkey.one1@gmail.com>
2022-01-25 00:37:38 +1000

    add shell script examples after testing on local

---
+++ scripts/commands/governance/create-role.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tsukid tx customgov proposal role create-role newrole "NewRole Description" --title="title" --description="description" --whitelist="1,2" --blacklist="3,4" --from=validator --keyring-backend=test --home=$HOME/.tsukid --chain-id=testing --fees=100ukex --yes
+
+tsukid query customgov proposals
+tsukid query customgov proposal 1
+
+tsukid tx customgov proposal vote 1 1 --from validator --keyring-backend=test --home=$HOME/.tsukid --chain-id=testing --fees=100ukex --yes 
+
+# check newly created role
+tsukid query customgov role 3
