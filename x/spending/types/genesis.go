971189019091547052c52079c1b404f91c814854
0xjingle <dbkey.one1@gmail.com>
2022-01-31 12:48:32 +1000

    complete protobuf changes, register messages, implement proto for tx and queries

--- x/spending/types/genesis.go
+++ x/spending/types/genesis.go
@@ -2,9 +2,5 @@ package types
 
 // DefaultGenesis returns the default CustomGo genesis state
 func DefaultGenesis() *GenesisState {
-	return &GenesisState{
-		Aliases: []*TokenAlias{
-			NewTokenAlias("KEX", "Tsuki", "", 6, []string{"ukex", "mkex"}),
-		},
-	}
+	return &GenesisState{}
 }
