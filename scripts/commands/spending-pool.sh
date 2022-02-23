511f8d5bd1e62f0bd516fd75e5c7cfc9c51d37cb
0xjingle <dbkey.one1@gmail.com>
2022-02-24 01:39:54 +1000

    add spending pool missing features

--- scripts/commands/spending-pool.sh
+++ scripts/commands/spending-pool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tsukid tx spending create-spending-pool --name="validator-rewards-pool" --claim-start=$(($(date -u +%s))) --claim-end=$(($(date -u +%s) + 200)) --expire=$(($(date -u +%s) + 200)) --token="ukex" --rate=0.1 --vote-quorum="33" --vote-period="60" --vote-enactment="30" --owner-roles="" --owner-accounts=$(tsukid keys show -a validator --keyring-backend=test --home=$HOME/.tsukid) --beneficiary-roles="2" --beneficiary-accounts="" --from=validator --chain-id=testing --fees=100ukex --keyring-backend=test --home=$HOME/.tsukid --yes  --broadcast-mode=block 
+tsukid tx spending create-spending-pool --name="validator-rewards-pool" --claim-start=$(($(date -u +%s))) --claim-end=0 --token="ukex" --rate=0.1 --vote-quorum="33" --vote-period="60" --vote-enactment="30" --owner-roles="" --owner-accounts=$(tsukid keys show -a validator --keyring-backend=test --home=$HOME/.tsukid) --beneficiary-roles="2" --beneficiary-accounts="" --from=validator --chain-id=testing --fees=100ukex --keyring-backend=test --home=$HOME/.tsukid --yes  --broadcast-mode=block 
 
 tsukid tx spending deposit-spending-pool --name="validator-rewards-pool" --amount=1000000ukex --from=validator --chain-id=testing --fees=100ukex --keyring-backend=test --home=$HOME/.tsukid --yes --broadcast-mode=block 
 
@@ -16,7 +16,7 @@ tsukid query customgov roles $(tsukid keys show -a validator --keyring-backend=t
 tsukid query bank balances $(tsukid keys show -a validator --keyring-backend=test --home=$HOME/.tsukid)
 
 # proposals
-tsukid tx spending proposal-update-spending-pool --title="title" --description="description" --name="validator-rewards-pool" --claim-start=$(($(date -u +%s))) --claim-end=$(($(date -u +%s) + 200)) --expire=$(($(date -u +%s) + 200)) --token="ukex" --rate=0.5 --vote-quorum="33" --vote-period="60" --vote-enactment="30" --owner-roles="" --owner-accounts=$(tsukid keys show -a validator --keyring-backend=test --home=$HOME/.tsukid) --beneficiary-roles="2" --beneficiary-accounts="" --from=validator --keyring-backend=test --home=$HOME/.tsukid --chain-id=testing --fees=100ukex --yes
+tsukid tx spending proposal-update-spending-pool --title="title" --description="description" --name="validator-rewards-pool" --claim-start=$(($(date -u +%s))) --claim-end=0 --token="ukex" --rate=0.5 --vote-quorum="33" --vote-period="60" --vote-enactment="30" --owner-roles="" --owner-accounts=$(tsukid keys show -a validator --keyring-backend=test --home=$HOME/.tsukid) --beneficiary-roles="2" --beneficiary-accounts="" --from=validator --keyring-backend=test --home=$HOME/.tsukid --chain-id=testing --fees=100ukex --yes
 tsukid tx spending proposal-spending-pool-withdraw --title="title" --description="description" --name="validator-rewards-pool" --beneficiary-accounts=$(tsukid keys show -a validator --keyring-backend=test --home=$HOME/.tsukid) --amount=210000ukex --from=validator --keyring-backend=test --home=$HOME/.tsukid --chain-id=testing --fees=100ukex --yes
 tsukid tx spending proposal-spending-pool-distribution --title="title" --description="description" --name="validator-rewards-pool" --from=validator --keyring-backend=test --home=$HOME/.tsukid --chain-id=testing --fees=100ukex --yes
 
