8317c563fba71c22c2fc06932096a2d7b1735bc6
0xjingle <dbkey.one1@gmail.com>
2022-03-13 05:39:42 +0000

    Add new build & publish scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,20 +3,30 @@ set -e
 set -x
 . /etc/profile
 
+LOCAL_PLATFORM="$(uname)" && LOCAL_PLATFORM="$(echo "$LOCAL_PLATFORM" |  tr '[:upper:]' '[:lower:]' )"
+LOCAL_ARCH=$(([[ "$(uname -m)" == *"arm"* ]] || [[ "$(uname -m)" == *"aarch"* ]]) && echo "arm64" || echo "amd64")
+LOCAL_OUT="${GOBIN}/tsukid"
+
+PLATFORM="$1" && [ -z "$PLATFORM" ] && PLATFORM="$LOCAL_PLATFORM"
+ARCH="$2" && [ -z "$ARCH" ] && ARCH="$LOCAL_ARCH"
+OUTPUT="$3" && [ -z "$OUTPUT" ] && OUTPUT="$LOCAL_OUT"
 
 CONSTANS_FILE=./types/constants.go
-TSUKI_VER=$(grep -Fn -m 1 'TsukiVersion ' $CONSTANS_FILE | rev | cut -d "=" -f1 | rev | xargs | tr -dc '[:alnum:]\-\.' || echo '')
-($(isNullOrEmpty "$TSUKI_VER")) && ( echoErr "ERROR: TsukiVersion was NOT found in contants '$CONSTANS_FILE' !" && sleep 5 && exit 1 )
+VERSION=$(grep -Fn -m 1 'TsukiVersion ' $CONSTANS_FILE | rev | cut -d "=" -f1 | rev | xargs | tr -dc '[:alnum:]\-\.' || echo '')
+($(isNullOrEmpty "$VERSION")) && ( echoErr "ERROR: TsukiVersion was NOT found in contants '$CONSTANS_FILE' !" && sleep 5 && exit 1 )
 
 COMMIT=$(git log -1 --format='%H') && \
 ldfName="-X github.com/cosmos/cosmos-sdk/version.Name=tsuki" && \
 ldfAppName="-X github.com/cosmos/cosmos-sdk/version.AppName=tsukid" && \
-ldfVersion="-X github.com/cosmos/cosmos-sdk/version.Version=$TSUKI_VER" && \
-ldfCommit="-X github.com/cosmos/cosmos-sdk/version.Name=$COMMIT"
+ldfVersion="-X github.com/cosmos/cosmos-sdk/version.Version=$VERSION" && \
+ldfCommit="-X github.com/cosmos/cosmos-sdk/version.Commit=$COMMIT" && \
+ldfBuildTags="-X github.com/cosmos/cosmos-sdk/version.BuildTags=${PLATFORM},${ARCH}"
 
-rm -rfv "${GOBIN}/tsukid" || echo "ERROR: Failed to wipe old tsukid binary"
+rm -fv "$OUTPUT" || echo "ERROR: Failed to wipe old tsukid binary"
 
 go mod tidy
 GO111MODULE=on go mod verify
-go build -ldflags "${ldfName} ${ldfAppName} ${ldfVersion} ${ldfCommit}" -o "${GOBIN}/tsukid" ./cmd/tsukid
-echoInfo "INFO: Sucessfully intalled TSUKI $(tsukid version)"
+env GOOS=$PLATFORM GOARCH=$ARCH go build -ldflags "${ldfName} ${ldfAppName} ${ldfVersion} ${ldfCommit} ${ldfBuildTags}" -o "$OUTPUT" ./cmd/tsukid
+
+( [ "$PLATFORM" == "$LOCAL_PLATFORM" ] && [ "$ARCH" == "$LOCAL_ARCH" ] && [ -f $OUTPUT ] ) && \
+    echoInfo "INFO: Sucessfully built TSUKI $($OUTPUT version)" || echoInfo "INFO: Sucessfully built TSUKI to '$OUTPUT'"
