11e57efb270ab45d32e45fbfaa9ad53260ed4f22
0xjingle <dbkey.one1@gmail.com>
2022-03-28 11:43:03 +1000

    distributor module bootstrap

---
+++ x/distributor/abci.go
@@ -0,0 +1,10 @@
+package distributor
+
+import (
+	"github.com/TsukiCore/tsuki/x/distributor/keeper"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
+
+}
