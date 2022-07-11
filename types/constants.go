c18e84e6704572db4ff4e5ddd567d496defc1a30
0xjingle <dbkey.one1@gmail.com>
2022-07-11 21:51:59 +0000

    tag name fix

--- types/constants.go
+++ types/constants.go
@@ -3,6 +3,6 @@ package types
 const (
 	// we set page iteration limit for safety
 	PageIterationLimit = 512
-	TsukiVersion       = "v0.2.3-rc.17"
+	TsukiVersion       = "v0.2.4-rc.18"
 	CosmosVersion      = "v0.45.1"
 )
