975905e56b3383b82b89fa115123f9a2d8d9d15e
0xy0k <dbkey.one1@gmail.com>
2023-01-25 20:19:31 +0100

    docs(protocol): add/remove . where it is fit from titles

--- packages/protocol/src/providers/mainnet/Euler.sol
+++ packages/protocol/src/providers/mainnet/Euler.sol
@@ -9,7 +9,7 @@ import {IEulerEToken} from "../../interfaces/euler/IEulerEToken.sol";
 import {IEulerDToken} from "../../interfaces/euler/IEulerDToken.sol";
 
 /**
- * @title Euler Finance Lending Provider.
+ * @title Euler Finance Lending Provider
  * @author everestdao Labs
  * @notice This contract allows interaction with Euler Finance.
  */
