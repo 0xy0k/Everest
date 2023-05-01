7e7a91af3f5a1de4fa08468dcf178cbb1a97ffee
0xjingle <dbkey.one1@gmail.com>
2023-05-01 11:03:17 +0200

    Custody module double address protection

--- types/constants.go
+++ types/constants.go
@@ -3,6 +3,6 @@ package types
 const (
 	// we set page iteration limit for safety
 	PageIterationLimit = 512
-	TsukiVersion       = "v0.3.6.30"
+	TsukiVersion       = "v0.3.7.31"
 	CosmosVersion      = "v0.45.10"
 )
