da416d869c862a5e0f81b96462e15931b5c08ca2
0xjingle <dbkey.one1@gmail.com>
2023-08-07 20:17:47 +0200

    #501 release

--- types/constants.go
+++ types/constants.go
@@ -3,6 +3,6 @@ package types
 const (
 	// we set page iteration limit for safety
 	PageIterationLimit = 512
-	TsukiVersion       = "v0.3.23"
+	TsukiVersion       = "v0.3.24"
 	CosmosVersion      = "v0.45.10"
 )
